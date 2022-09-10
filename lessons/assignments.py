f9ea82b31715c5c67f96e6924126f20f6a67370a
Coburn <coburncodes@gmail.com>
2022-09-09 18:35:49 -0700

    Add final assignment

--- lessons/assignments.py
+++ lessons/assignments.py
@@ -586,6 +586,8 @@ def x15_2():
 
 
 def x15_3():
+    print("Create and populate 4 tables with non-repeating entries in a database using iTunes XML file.\n")
+    input("Press enter to continue...")
     conn = sqlite3.connect('trackdb.sqlite')
     cur = conn.cursor()
 
@@ -659,20 +661,91 @@ def x15_3():
         cur.execute("INSERT OR IGNORE INTO Artist (name) VALUES ( ? )", (artist,))
         cur.execute("SELECT id FROM Artist WHERE name = ? ", (artist, ))
         artist_id = cur.fetchone()[0]
-        print("artist_id:", artist_id)
 
         cur.execute("INSERT OR IGNORE INTO Album (title, artist_id) VALUES ( ?, ? )", (album, artist_id))
         cur.execute("SELECT id FROM Album WHERE title = ? ", (album, ))
         album_id = cur.fetchone()[0]
-        print("album_id:", album_id)
 
         cur.execute("INSERT OR IGNORE INTO Genre (name) VALUES ( ? )", (genre,))
         cur.execute("SELECT id FROM Genre WHERE name = ? ", (genre, ))
         genre_id = cur.fetchone()[0]
-        print("genre_id:", genre_id)
 
         cur.execute("INSERT OR REPLACE INTO Track (title, album_id, genre_id, len, rating, count) VALUES ( ?, ?, ?, ?, ?, ? )", (name, album_id, genre_id, length, rating, count))
 
         conn.commit()
+
+def x15_4():
+    print("Retrieve string from JSON data based on query.\n")
+    input("Press enter to continue...")
+
+    conn = sqlite3.connect('rosterdb.sqlite')
+    cur = conn.cursor()
+
+    # Do some setup
+    cur.executescript('''
+    DROP TABLE IF EXISTS User;
+    DROP TABLE IF EXISTS Member;
+    DROP TABLE IF EXISTS Course;
+
+    CREATE TABLE User (
+        id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+        name   TEXT UNIQUE
+    );
+
+    CREATE TABLE Course (
+        id     INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+        title  TEXT UNIQUE
+    );
+
+    CREATE TABLE Member (
+        user_id     INTEGER,
+        course_id   INTEGER,
+        role        INTEGER,
+        PRIMARY KEY (user_id, course_id)
+    )
+    ''')
+
+    fname = input('Enter file name: ')
+    if len(fname) < 1:
+        fname = 'roster_data.json'
+
+    # [
+    #   [ "Charley", "si110", 1 ],
+    #   [ "Mea", "si110", 0 ],
+
+    str_data = open(fname).read()
+    json_data = json.loads(str_data)
+
+    for entry in json_data:
+        name = entry[0]
+        title = entry[1]
+        role = int(entry[2])
+
+        cur.execute('''INSERT OR IGNORE INTO User (name)
+            VALUES ( ? )''', ( name, ) )
+        cur.execute('SELECT id FROM User WHERE name = ? ', (name, ))
+        user_id = cur.fetchone()[0]
+
+        cur.execute('''INSERT OR IGNORE INTO Course (title)
+            VALUES ( ? )''', ( title, ) )
+        cur.execute('SELECT id FROM Course WHERE title = ? ', (title, ))
+        course_id = cur.fetchone()[0]
+
+        cur.execute('''INSERT OR REPLACE INTO Member
+            (user_id, course_id, role) VALUES ( ?, ?, ? )''',
+            ( user_id, course_id, role ) )
+
+        conn.commit()
+
+    data = cur.execute('''SELECT 'XYZZY' || hex(User.name || Course.title || Member.role ) AS X FROM 
+        User JOIN Member JOIN Course 
+        ON User.id = Member.user_id AND Member.course_id = Course.id
+        ORDER BY X LIMIT 1''')
+
+    readable = data.fetchone()[0]
+    print(readable)
+
+    cur.close()
+
 
 main()
